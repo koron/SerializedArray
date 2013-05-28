2bbe2a6c1de2fe8c32e40341d9d57e9c102c2364
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-05-28 17:39:43 +0900

    first import

---
+++ src/main/java/net/kaoriya/qb/serialized_array/App.java
@@ -0,0 +1,13 @@
+package net.kaoriya.qb.serialized_array;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
